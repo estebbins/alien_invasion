16130699af4cfb44bf3a910ef78f49ab3a5e1d01
estebbins <sc19090483882882828@yahoo.com>
2022-10-25 12:47:29 -0400

    repeat bullets and mega bullets now set up as desired and added to repository 1245pm 10_25_2022

---
+++ mega_bullet.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.sprite import Sprite
+
+class MegaBullet(Sprite):
+    """a class to manage bullets fired from the ship"""
+    def __init__(self, ai_game):
+        """create a bullet object at the ship's current position"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        #import bullet image
+        self.image = pygame.image.load('images/mega_bullet.bmp')
+
+        #createa  bullet rect at (0, 0) and then set correct position
+        self.rect = pygame.Rect(0,0, 24, 60)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        #store the bullet's position as a decimal value
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """move the bullet up the screen"""
+        #update the decimal position of the bullet
+        self.y -= self.settings.mega_bullet_speed
+        #Update the rect position.
+        self.rect.y = self.y
+
+    def draw_mega_bullet(self):
+        """draw the bullet at it's current location"""
+        self.screen.blit(self.image, self.rect)
