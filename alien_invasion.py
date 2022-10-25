de4cf62ae60006b5c8e5c3767cfa4a078395a43e
estebbins <sc19090483882882828@yahoo.com>
2022-10-25 11:05:25 -0400

    Key left and right settings created, pre full screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,16 +30,46 @@ class AlienInvasion:
             #to access the events that pygame detects uses the function below
             #which returns a list of events that have taken place since the
             #last time the fucntion was called
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            #redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
-            #make the most recently drawn screen visible. 
-            pygame.display.flip()
+    def _check_keydown_events(self, event):
+        """respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            #move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """update images on the screen flip to a new screen"""
+        #redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #make the most recently drawn screen visible. 
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     #Make a game instance and run the game.
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """initialize the ship and set it's starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get it's rect.
@@ -14,6 +15,24 @@ class Ship:
 
         #Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        #movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ships position based on the movement flag"""
+        #Update the ships x value not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left >0:
+            self.x -= self.settings.ship_speed
+
+        #update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """draw the ship at it's current location"""
