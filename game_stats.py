f7d403b6c38baf04fd0019772d954db01a52ae4a
estebbins <sc19090483882882828@yahoo.com>
2022-10-25 17:11:54 -0400

    Added high score 5:11pm 10/25/22:

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,9 @@ class GameStats:
         #start alien invasion in an active state
         self.game_active = False
 
+        #High score should never be reset.
+        self.high_score = 0
+
     def reset_stats(self):
         """initialize statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
